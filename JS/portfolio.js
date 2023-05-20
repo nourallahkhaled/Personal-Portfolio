4eb304c38edba8dccdaf40fbb3b3f527637b416d
nourallahkhaled <nourallahkhaled99@gmail.com>
2023-05-21 01:13:21 +0300

    merged home-portfolio

--- JS/portfolio.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', () => {
-    var left_side = document.getElementsByClassName('left-side')[0];
-    var right_side = document.getElementsByClassName('right-side')[0];
-    left_side.classList.toggle('scrolled', window.scrollY > 0);
-    right_side.classList.toggle('scrolled', window.scrollY > 0);
-})
