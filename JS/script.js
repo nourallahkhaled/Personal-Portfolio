dc3b349997c7feeade3888d18e23ed5eb95c1300
nourallahkhaled <nourallahkhaled99@gmail.com>
2023-05-21 02:16:54 +0300

    merged home-about-portfolio

--- JS/script.js
+++ JS/script.js
@@ -72,3 +72,35 @@ window.addEventListener('scroll', () => {
     right_side.classList.toggle('scrolled', window.scrollY > 0);
     fixed.classList.toggle('scrolled', window.scrollY>0);
 })
+
+//ABOUT 
+const links = document.querySelectorAll('.myLink');
+const paragraphs = document.querySelectorAll('.tab_item');
+
+links.forEach((link, index) => {
+    link.addEventListener('click', function () {
+        // Hide all paragraphs
+        paragraphs.forEach(paragraph => {
+            paragraph.style.display = 'none';
+        });
+
+        // Show the selected paragraph
+        paragraphs[index].style.display = 'block';
+    });
+});
+
+
+// Loop through the nav links
+links.forEach(link => {
+    // Add a click event listener to each link
+    link.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent the link from navigating to a new page
+
+        // Remove the active class from the current link
+        const currentLink = document.querySelector('.active');
+        currentLink.classList.remove('active');
+
+        // Add the active class to the clicked link
+        this.classList.add('active');
+    });
+});
